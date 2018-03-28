b96e02ffd247564ae1947cf157bd1a86a7f76850
Hedwigg <Hedwigg01@gmail.com>
2018-03-28 09:39:43 -0600

    Testing Eclipse Github controls

--- csci305.javalab/src/csci305/javalab/Outcome.java
+++ csci305.javalab/src/csci305/javalab/Outcome.java
@@ -1,5 +1,31 @@
 package csci305.javalab;
 
 public class Outcome {
-
+	public String first;
+	public String second;
+	
 }
+
+
+/*Outcome has two instance variables representing the outcome of the compareTo method.
+
+The first value contained in the Outcome class contains a string describing one of the following outcomes:
+
+Scissors cut Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitate Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+Rock crushes Scissors
+For a tie, you should output a string such as ``Rock equals Rock''.
+
+The second value the Outcome should contain the value of the round outcome:
+
+Win
+Lose
+Tie
+*/
