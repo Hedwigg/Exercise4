996e3f26699081258092d404448bf66fa589a2d8
Hedwigg <Hedwigg01@gmail.com>
2018-04-04 09:08:08 -0600

    Finished first version of "outcome" class.

--- csci305.javalab/src/csci305/javalab/Outcome.java
+++ csci305.javalab/src/csci305/javalab/Outcome.java
@@ -5,31 +5,138 @@ public class Outcome {
 	public String second;
 	
 	public Outcome(Lizard lizard, Element x) {
-		if (x.getName().equals("Paper"))
+		switch(x.getName())
 		{
-			first = "Lizard Eats Paper";
-			second = "Win";
-		}else if (x.getName().equals("Spock"))
-		{
-			first = "Lizard poisons Spock";
-			second = "Win";
-		}else if(x.getName().equals())
+			case "Paper":
+				first = "Lizard Eats Paper";
+				second = "Win";
+				
+			case "Spock":
+				first = "Lizard poisons Spock";
+				second = "Win"; 
+				
+			case "Rock":
+				first = "Lizard poisons Spock";
+				second = "Lose";
+				
+			case "Sissors":
+				first = "Sissors decapitate Lizard";
+				second = "Lose";
+			case "Lizard":
+				first = "Lizard equals Lizard";
+				second = "Tie";
+			default:
+				first = "error";
+				second = "error";
+		}
 	}
 
 	public Outcome(Paper paper, Element x) {
-		// TODO Auto-generated constructor stub
+		switch(x.getName())
+		{
+			case "Rock":
+				first = "Paper covers Rock";
+				second = "Win";
+				
+			case "Spock":
+				first = "Paper disproves Spock";
+				second = "Win"; 
+				
+			case "Lizard":
+				first = "Lizard eats Paper";
+				second = "Lose";
+				
+			case "Sissors":
+				first = "Sissors cut Paper";
+				second = "Lose";
+			case "Paper":
+				first = "Paper equals Paper";
+				second = "Tie";
+			default:
+				first = "error";
+				second = "error";
+		}
 	}
 
 	public Outcome(Rock rock, Element x) {
-		// TODO Auto-generated constructor stub
+		switch(x.getName())
+		{
+			case "Sissors":
+				first = "Rock crushes Sissors";
+				second = "Win";
+				
+			case "Lizard":
+				first = "Rock crushes Lizard";
+				second = "Win"; 
+				
+			case "Paper":
+				first = "Paper covers Rock";
+				second = "Lose";
+				
+			case "Spock":
+				first = "Spock vaporizes Rock";
+				second = "Lose";
+			case "Rock":
+				first = "Rock equals Rock";
+				second = "Tie";
+			default:
+				first = "error";
+				second = "error";
+		}
 	}
 
 	public Outcome(Scissors scissors, Element x) {
-		// TODO Auto-generated constructor stub
+		switch(x.getName())
+		{
+			case "Paper":
+				first = "Sissors cut Paper";
+				second = "Win";
+				
+			case "Lizard":
+				first = "Sissors decapitate Lizard";
+				second = "Win"; 
+				
+			case "Rock":
+				first = "Rock crushes Sissors";
+				second = "Lose";
+				
+			case "Spock":
+				first = "Spock smashes Sissors";
+				second = "Lose";
+			case "Sissors":
+				first = "Sissors equals Sissors";
+				second = "Tie";
+			default:
+				first = "error";
+				second = "error";
+		}
 	}
 
 	public Outcome(Spock spock, Element x) {
-		// TODO Auto-generated constructor stub
+		switch(x.getName())
+		{
+			case "Sissors":
+				first = "Spock smashes Sissors";
+				second = "Win";
+				
+			case "Rock":
+				first = "Spock vaporizes Rock";
+				second = "Win"; 
+				
+			case "Lizard":
+				first = "Lizard poisons Spock";
+				second = "Lose";
+				
+			case "Paper":
+				first = "Paper disproves Spock";
+				second = "Lose";
+			case "Spock":
+				first = "Spock equals Spock";
+				second = "Tie";
+			default:
+				first = "error";
+				second = "error";
+		}
 	}
 	
 }
